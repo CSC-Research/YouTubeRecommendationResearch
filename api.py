b43b2e8dfa17b10c7f289f6c0e26a11a5cf0c0a7
svmadiraju <saisujit25@gmail.com>
2020-02-21 13:09:40 -0800

    Improved

--- api.py
+++ api.py
@@ -99,13 +99,8 @@ if __name__ == '__main__':
 	parser.add_argument('--max-results', help='Max results', default=25)
 	args = parser.parse_args()
 
-	try:
-		# videoArr = vidInfoRequest(BASE_VIDEO_ID)
-		v1 = Video(BASE_VIDEO_ID)
-		v1Info = v1.toString()
+	v1 = Video(BASE_VIDEO_ID)
+	v1Info = v1.toString()
 
-		with open('output.txt', 'w') as fp:
-			fp.write(v1Info.encode('utf8'))
-
-	except HttpError, e:
-		print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
+	with open('output.txt', 'w') as fp:
+		fp.write(v1Info.encode('utf8'))
